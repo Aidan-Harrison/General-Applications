ed879ad55f4d9289ad9a405951b685438f2fe78f
COVVA <34375218+COVVA@users.noreply.github.com>
2021-02-13 18:20:56 +0000

    Perk System Re-write
    New version allows for easy expansion, better readability, better efficiency and overall more professional

--- Perk System/Perk System/Perk System/Main.cpp
+++ Perk System/Perk System/Perk System/Main.cpp
@@ -1,103 +1,54 @@
 #include "Player.h"
 #include <ctime>
-#include "Setup.h"
-
-using namespace PerkSetup;
-using namespace WeaponSetup;
 
 Player player;
 void Interface();
 
-void PrintStats() {
-	std::cout << "\nPlayer name: " << player.name << '\n';
-	std::cout << "Points: " << player.points << '\n';
-	std::cout << "Perks:\n";
-	for (unsigned int i = 0; i < player.perkInventory.size(); i++)
-		std::cout << player.perkInventory[i].perkName << '\n';
-	std::cout << "Weapons:\n";
-	for (unsigned int i = 0; i < player.weaponInventory.size(); i++)
-		std::cout << player.weaponInventory[i].wepName << '\n';
-}
-
 void RollPerk() {
 	short input = 0;
-	std::cout << "Pick a perk:\n";
-	std::cout << "1) Juggernaut  2) Speed-Cola  3) Double-Tap";
+	std::cout << "Pick a perk:\n" <<
+				 "1) Juggernaut  2) Speed - Cola  3) Double - Tap";
 	std::cin >> input;
-	if (input == 1) {
-		if (player.points >= 2500) {
-			player.points -= 2500;
-			player.perkInventory.push_back(Juggernaut);
-		}
-		else {
-			std::cout << "You do not have enough points for Juggernaut!\n";
-			std::cin.get();
-			std::cin.get();
-			Interface();
-		}
-	}
-	else if(input == 2) {
-		if (player.points >= 2000) {
-			player.points -= 2000;
-			player.perkInventory.push_back(DoubleTap);
-		}
-		else {
-			std::cout << "You do not have enough points for Double-Tap!\n";
-			std::cin.get();
-			std::cin.get();
-			Interface();
-		}
-	}
-	else if(input == 3) {
-		if (player.points >= 3000) {
-			player.points -= 3000;
-			player.perkInventory.push_back(SpeedCola);
-		}
-		else {
-			std::cout << "You do not have enough points for Speed-Cola!\n";
-			std::cin.get();
-			std::cin.get();
-			Interface();
-		}
-	}
+	if(input == 1 && player.points >= 2500)		 player.AddPerk(1);
+	else if(input == 2 && player.points >= 2000) player.AddPerk(2);
+	else if(input == 3 && player.points >= 3000) player.AddPerk(3);
+	else if(input == 4 && player.points >= 3500) player.AddPerk(4);
+	else std::cout << "You do not have enough points for "; // Add perk name
+	std::cin.get();
 	Interface();
 }
 
 void MysteryBox() {
-	if (player.points >= 950) {
+	if (player.points >= 950 && player.weaponInventory.size() != player.maxWeapons) {
 		player.points -= 950;
 		srand(time(0));
 		int weapon = 0;
 		weapon = rand() % 3;
 		switch (weapon) {
-			case 1: player.weaponInventory.push_back(FiveSeven); break;
-			case 2: player.weaponInventory.push_back(Olympus);   break;
-			case 3: player.weaponInventory.push_back(RPK);       break;
+			case 1: player.AddWeapon(1); break;
+			case 2: player.AddWeapon(2); break;
+			case 3: player.AddWeapon(3); break;
 		}
 	}
-	else {
-		std::cout << "You don't have enough points!";
-		std::cin.get();
-		std::cin.get();
-	}
+	else std::cout << "You don't have enough points or you are already at max weapons!";
+	std::cin.get();
 	Interface();
 }
 
 void Interface() {
 	system("cls");
 	short input = 0;
-	std::cout << "Zombie's System:\n";
-	std::cout << "Pick an option | 1) Perk  2) Mystery Box  3) Print Stats"; 
+	std::cout << "Zombie's System:\n" <<
+				 "Pick an option | 1) Perk  2) Mystery Box  3) Print Stats"; 
 	std::cin >> input;
 	switch (input) {
-	case 1: RollPerk();   break;
-	case 2: MysteryBox(); break;
-	case 3: PrintStats(); break;
+		case 1: RollPerk();   break;
+		case 2: MysteryBox(); break;
+		case 3: player.PrintStats(); break;
 	}
 }
 
 int main() {
-	std::cout << "Enter a name: "; std::cin >> player.name;
 	std::cout << "How many points would you like to start with? (Defaults to 500) "; std::cin >> player.points;
 	Interface();
 	return 0;
--- Perk System/Perk System/Perk System/Player.h
+++ Perk System/Perk System/Perk System/Player.h
@@ -2,24 +2,24 @@
 #define Player_h
 
 #include <vector>
-#include "Perks.h"
-#include "Weapons.h"
+#include "Setup.h"
 
 class Player {
 private:
 public:
-	short points = 500;
+	short points = 500, maxWeapons = 3;
 	short m_Health, m_Stamina, m_Damage, m_ReloadSpeed;
 	std::vector<Perks> perkInventory{};
 	std::vector<Weapon> weaponInventory{};
-	std::string name = "";
 	Player(short health = 100, short stamina = 100, short damage = 10, short reloadSpeed = 5)
 		:m_Health(health), m_Stamina(stamina), m_Damage(damage), m_ReloadSpeed(reloadSpeed)
 	{
 		assert(m_Health != 0 && m_Stamina != 0 && m_Damage != 0 && m_ReloadSpeed != 0);
 	}
 	~Player() = default;
-	void AddPerk();
+	void AddPerk(short perk);
+	void AddWeapon(short weapon);
+	void PrintStats();
 };
 
 #endif
